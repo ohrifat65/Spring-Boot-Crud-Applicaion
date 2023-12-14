a3eb3bd9ff4922f0855fcc5160386b0058aa7588
Oh Rifat <60514802+ohrifat65@users.noreply.github.com>
2023-12-14 20:43:46 +0600

    Update empController.java
    save option added

--- Employee/src/main/java/com/crud/employee/controller/empController.java
+++ Employee/src/main/java/com/crud/employee/controller/empController.java
@@ -20,6 +20,13 @@ public class empController {
 		return "employeeReg.html";
 	}
 
+	//Save 
+	@PostMapping("/submitForm")
+	public String Save(@ModelAttribute empModel ma) {
+		repo.save(ma);
+		return "info.html";
+	}
+	
 	@PostMapping("/updateForm")
 	public String UpdateF(@RequestParam int eId, @ModelAttribute empModel m) {
 		empModel eModel = repo.getOne(eId);
